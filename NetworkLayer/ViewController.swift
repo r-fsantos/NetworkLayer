f60b0625b84ab097bf1e77b6dd3d36b89ddad10c
renato <felicio95.jr@gmail.com>
2022-04-08 11:06:57 -0300

    Initial Commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Renato F. dos Santos Jr on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
